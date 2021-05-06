a3567e67008b837c057df0f6288f90472ce56fee
tigerich <tigerich@163.com>
2021-05-07 02:29:11 +0800

    update plot

--- roles/deploy-java/files/plot.sh
+++ roles/deploy-java/files/plot.sh
@@ -2,9 +2,3 @@ ssd_path=/media/ray/ssd
 data_path=/media/ray/data1
 
 chia plots create -n1 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
-chia plots create -n1 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
-chia plots create -n1 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
-chia plots create -n1 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
-chia plots create -n1 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
-chia plots create -n1 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
-chia plots create -n1 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
