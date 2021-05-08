c0ac96dc22d9a7ba6fc5ddf3108e36ed00ad0577
tigerich <tigerich@163.com>
2021-05-08 17:53:07 +0800

    update autossh

--- roles/deploy-chia/files/add_jump_ssh_tunnel.sh
+++ roles/deploy-chia/files/add_jump_ssh_tunnel.sh
@@ -1,2 +1,9 @@
+# ssh -fN -R hn-jump:10022:localhost:22 richardchen@hn-jump 
 
-autossh -M 10021 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R hn-jump:10022:localhost:22 ray@hn-jump
+autossh -M 10021 -fN \
+  -o "PubkeyAuthentication=yes" \
+  -o "StrictHostKeyChecking=false" \
+  -o "PasswordAuthentication=no" \
+  -o "ServerAliveInterval 60" \
+  -o "ServerAliveCountMax 3" \
+  -R hn-jump:10022:localhost:22 richardchen@hn-jump
