7a156217fa1d1b3251e43e5d273435f8157e86bf
tigerich <tigerich@163.com>
2021-05-08 14:58:39 +0800

    update

--- roles/deploy-chia/files/chia_plot_data1.sh
+++ roles/deploy-chia/files/chia_plot_data1.sh
@@ -1,4 +1,4 @@
 ssd_path=/media/ray/ssd
 data_path=/media/ray/data1
 
-sudo chia plots create -n10 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
+chia plots create -n10 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
--- roles/deploy-chia/files/chia_plot_data2.sh
+++ roles/deploy-chia/files/chia_plot_data2.sh
@@ -1,4 +1,4 @@
 ssd_path=/media/ray/ssd
 data_path=/media/ray/data2
 
-sudo chia plots create -n10 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
+chia plots create -n10 -k32 -b4000 -u128 -r3 -t $ssd_path -d $data_path
