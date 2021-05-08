626cc4f78057bbae87dc4fa965c3930a5ffdbb91
tigerich <tigerich@163.com>
2021-05-08 17:40:36 +0800

    add ssh jump tunnel

---
+++ roles/deploy-chia/files/add_ssh_rev_proxy.sh
@@ -0,0 +1,2 @@
+
+autossh -M 10021 -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R hn-jump:10022:localhost:22 ray@hn-jump
