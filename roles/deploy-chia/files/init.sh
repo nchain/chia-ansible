21a1ac96b611b3324a333c3ed052eacb21e779d7
tigerich <tigerich@163.com>
2021-05-10 20:59:30 +0800

    update

--- roles/deploy-chia/files/init.sh
+++ roles/deploy-chia/files/init.sh
@@ -4,7 +4,7 @@ sudo mkdir -p /media/ray/ssd && sudo chown ray:ray /media/ray/ssd
 sudo mkdir -p /media/ray/data1 && sudo chown ray:ray /media/ray/data1
 sudo mkdir -p /media/ray/data2 && sudo chown ray:ray /media/ray/data2
 
-cd ~/chia-ansible 
+cd ~/chia-blockhain
 git remote set-url origin git@github.com:Chia-Network/chia-blockchain.git
 git pull
 git checkout tags/1.1.5
@@ -12,3 +12,7 @@ git checkout tags/1.1.5
 sh install.sh
 . ./activate
 chia init
+
+mkdir -p ~/mnes
+
+chia keys add -f /home/ray/mnes/{{ ansible_host }}.mne
