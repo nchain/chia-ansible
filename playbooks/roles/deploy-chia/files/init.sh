0b491002ccafab814dfbc3ab5207658db65b4370
tigerich <tigerich@163.com>
2021-05-10 23:23:33 +0800

    fix typo chia-blockchain

--- playbooks/roles/deploy-chia/files/init.sh
+++ playbooks/roles/deploy-chia/files/init.sh
@@ -9,7 +9,7 @@ sudo mkdir -p /media/ray/ssd && sudo chown ray:ray /media/ray/ssd
 sudo mkdir -p /media/ray/data1 && sudo chown ray:ray /media/ray/data1
 sudo mkdir -p /media/ray/data2 && sudo chown ray:ray /media/ray/data2
 
-cd ~/chia-blockhain
+cd ~/chia-blockchain
 git remote set-url origin git@github.com:Chia-Network/chia-blockchain.git
 git pull
 git checkout tags/1.1.5
