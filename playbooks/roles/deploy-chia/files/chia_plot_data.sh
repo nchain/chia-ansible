069ba0867c38b997ed6b91c6110ffdc6ad5754b8
tigerich <tigerich@163.com>
2021-05-11 10:29:21 +0800

    update

--- playbooks/roles/deploy-chia/files/chia_plot_data.sh
+++ playbooks/roles/deploy-chia/files/chia_plot_data.sh
@@ -2,4 +2,4 @@ ssd_path=/media/ray/ssd
 data_path=/media/ray/$1
 plot_n=$2
 
-chia plots create -n $plot_n -k32 -b4300 -u128 -r3 -t $ssd_path -d $data_path >/dev/null 2>&1 &
+chia plots create -n $plot_n -k32 -b4300 -u128 -r3 -t $ssd_path -d $data_path >/dev/null 2>&1
