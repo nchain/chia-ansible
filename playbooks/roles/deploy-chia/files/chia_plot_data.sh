ab54bed28eb6859412bb10306a3bd99c1f44486a
tigerich <tigerich@163.com>
2021-05-11 10:49:03 +0800

    update plot script

--- playbooks/roles/deploy-chia/files/chia_plot_data.sh
+++ playbooks/roles/deploy-chia/files/chia_plot_data.sh
@@ -2,4 +2,7 @@ ssd_path=/media/ray/ssd
 data_path=/media/ray/$1
 plot_n=$2
 
+cd ~/chia-blockchain
+. ./activate
+
 chia plots create -n $plot_n -k32 -b4300 -u128 -r3 -t $ssd_path -d $data_path >/dev/null 2>&1
