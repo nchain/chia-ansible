64c39c99889d8ac6322a9e5037e80fb7ce2bbf22
tigerich <tigerich@163.com>
2021-05-14 16:09:42 +0800

    update jumpstart

--- playbooks/roles/deploy-chia/files/jumpstart.sh
+++ playbooks/roles/deploy-chia/files/jumpstart.sh
@@ -2,16 +2,16 @@
 
 case "$(pidof autossh | wc -w)" in
 
-0)  echo "Restarting autossh for to hn-jump:     $(date)" >> /var/log/jumpstart.txt
+0)  echo "Restarting autossh for to hn-jump:     $(date)" >> /tmp/jumpstart.log
     sh add_jump_ssh_tunnel.sh &
     ;;
 1)  # all ok
     ;;
-*)  echo "Removed extra autossh: $(date)" >> /var/log/jumpstart.txt
+*)  echo "Removed extra autossh: $(date)" >> /tmp/jumpstart.log
     kill $(pidof autossh | awk '{print $1}')
     ;;
 esac
 
 ### cronjob config (execute every 5 mins)
-## */5 * * * * /home/ray/jumpstart.sh
+## */1 * * * * /home/ray/jumpstart.sh
 ###
