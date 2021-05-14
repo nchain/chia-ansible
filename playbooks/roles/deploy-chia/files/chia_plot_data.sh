c0c89a8f53f6c06b296879f55ac0f40a8e9e8daf
tigerich <tigerich@163.com>
2021-05-14 19:35:36 +0800

    fix plot data bin

--- playbooks/roles/deploy-chia/files/chia_plot_data.sh
+++ playbooks/roles/deploy-chia/files/chia_plot_data.sh
@@ -2,7 +2,7 @@ ssdPath=/media/ray/$1
 dataPath=/media/ray/$2
 roundNum=$3     # round num
 taskRam=$4      # ram size per task
-kNum=$4         # file format E.g. k32
+kNum=$5         # file format E.g. k32
 
 rNum=2          # threads per task
 
@@ -10,4 +10,4 @@ cd ~/chia-blockchain
 . ./activate
 
 echo chia plots create -n $roundNum -k $kNum -b $taskRam -u 128 -r $rNum \
-    -t $ssdPath -d $dataPath >/dev/null 2>&1 >> run-cmd.sh
+    -t $ssdPath -d $dataPath >/dev/null 2>&1
