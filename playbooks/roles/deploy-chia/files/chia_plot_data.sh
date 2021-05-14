0c7b2250b5a544d6ae2bb61ca913cad2feb91ba0
tigerich <tigerich@163.com>
2021-05-14 18:40:54 +0800

    update

--- playbooks/roles/deploy-chia/files/chia_plot_data.sh
+++ playbooks/roles/deploy-chia/files/chia_plot_data.sh
@@ -1,13 +1,13 @@
 ssdPath=/media/ray/$1
 dataPath=/media/ray/$2
-plotNum=$3
-kNum=$4
-taskRam=$5
+roundNum=$3     # round num
+kNum=$4         # file format E.g. k32
+taskRam=$5      # ram size per task
 
-rNum=2
+rNum=2          # threads per task
 
 cd ~/chia-blockchain
 . ./activate
 
-chia plots create -n $plotNum -k $kNum -b $taskRam -u 128 -r $rNum \
+chia plots create -n $roundNum -k $kNum -b $taskRam -u 128 -r $rNum \
     -t $ssdPath -d $dataPath >/dev/null 2>&1
