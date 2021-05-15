7237c4d91c856483cce564344ff80365735c4ec7
tigerich <tigerich@163.com>
2021-05-15 11:38:39 +0800

    update

--- playbooks/roles/deploy-chia/files/chia_plot_data.sh
+++ playbooks/roles/deploy-chia/files/chia_plot_data.sh
@@ -9,7 +9,6 @@ rNum=2          # threads per task
 cd ~/chia-blockchain
 . ./activate
 
-echo "chia plots create -n$roundNum -k$kNum -b$taskRam -u128 -r$rNum -t $ssdPath -d $dataPath > /dev/null 2>&1 &" >> ~/$host/chia.log
+echo "chia plots create -n$roundNum -k$kNum -b$taskRam -u128 -r$rNum -t $ssdPath -d $dataPath" >> ~/$host/chia.log
 
-chia plots create -n$roundNum -k$kNum -b$taskRam -u128 -r$rNum \
-    -t $ssdPath -d $dataPath > /dev/null 2>&1 &
+chia plots create -n$roundNum -k$kNum -b$taskRam -u128 -r$rNum -t $ssdPath -d $dataPath
