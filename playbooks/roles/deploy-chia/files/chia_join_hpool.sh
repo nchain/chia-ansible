2c6207320d34ad0c2927607902d526c29e0ae572
tigerich <tigerich@163.com>
2021-05-16 16:26:18 +0800

    update

--- playbooks/roles/deploy-chia/files/chia_join_hpool.sh
+++ playbooks/roles/deploy-chia/files/chia_join_hpool.sh
@@ -1,5 +1,7 @@
 conf=$1
 
-hpool_bin=~/chia-ansible/playbooks/roles/deploy-chia/files/hpool/hpool-miner-chia
+hpool_dir=~/chia-ansible/playbooks/roles/deploy-chia/files/hpool
+hpool_bin=$hpool_dir/hpool-miner-chia
+hpool_conf=$hpool_dir/$conf
 
-$hpool_bin -config $conf >/dev/null 2>&1 &
+$hpool_bin -config $hpool_conf >/dev/null 2>&1 &
