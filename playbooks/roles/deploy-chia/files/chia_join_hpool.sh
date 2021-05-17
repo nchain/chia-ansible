17a9162239b1ecc11712c3a18ae49667aa59cfbb
tigerich <tigerich@163.com>
2021-05-17 16:45:56 +0800

    refine joinpool

--- playbooks/roles/deploy-chia/files/chia_join_hpool.sh
+++ playbooks/roles/deploy-chia/files/chia_join_hpool.sh
@@ -1,9 +1,10 @@
 host=$1
 
+function join_pool {
+    echo "cd ~/$host && ./hpool-miner-chia -config ./hpool_conf.yml" >> /tmp/hpool.log
+    cd ~/$host
+    ./hpool-miner-chia -config ./hpool_conf.yml
+}
 
 hpool_cnt=$(ps aux |grep chia_join_hpool | wc -l)
-[[ "$hpool_cnt" -gt 1 ]] && exit 0
-
-echo "cd ~/$host && ./hpool-miner-chia -config ./hpool_conf.yml" >> /tmp/hpool.log
-cd ~/$host
-./hpool-miner-chia -config ./hpool_conf.yml
+[[ "$hpool_cnt" -eq 1 ]] && join_pool
