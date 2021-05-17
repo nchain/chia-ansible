84f809a178137b3ac6dd63dfa91d57cff50ecda1
tigerich <tigerich@163.com>
2021-05-17 15:24:09 +0800

    update hpool

--- playbooks/roles/deploy-chia/files/chia_join_hpool.sh
+++ playbooks/roles/deploy-chia/files/chia_join_hpool.sh
@@ -1,5 +1,9 @@
 host=$1
 
+
+hpool_cnt=$(pidof chia_join_hpool | wc -w)
+[[ $hpool_cnt -eq 0 ]] && exit 0
+
 echo "cd ~/$host && ./hpool-miner-chia -config ./hpool_conf.yml" >> /tmp/hpool.log
 cd ~/$host
 ./hpool-miner-chia -config ./hpool_conf.yml
