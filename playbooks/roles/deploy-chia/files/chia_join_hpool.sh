b08be4d5d1456b506de505197cbbd035418d4ef2
tigerich <tigerich@163.com>
2021-05-17 15:57:24 +0800

    fix join pool

--- playbooks/roles/deploy-chia/files/chia_join_hpool.sh
+++ playbooks/roles/deploy-chia/files/chia_join_hpool.sh
@@ -2,7 +2,7 @@ host=$1
 
 
 hpool_cnt=$(ps aux |grep chia_join_hpool | wc -l)
-[[ $hpool_cnt -gt 1 ]] && exit 0
+[[ "$hpool_cnt" -gt 1 ]] && exit 0
 
 echo "cd ~/$host && ./hpool-miner-chia -config ./hpool_conf.yml" >> /tmp/hpool.log
 cd ~/$host
