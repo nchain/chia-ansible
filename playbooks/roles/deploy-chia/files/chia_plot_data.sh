f6eacb16305014644f24e3fdbe49c5d4a4b422f3
tigerich <tigerich@163.com>
2021-05-18 20:08:14 +0800

    add batch plotting

--- playbooks/roles/deploy-chia/files/chia_plot_data.sh
+++ playbooks/roles/deploy-chia/files/chia_plot_data.sh
@@ -3,22 +3,26 @@
 kNum=$1         # file format E.g. k32
 roundNum=$2     # round num
 taskRam=$3      # ram size per task, E.g. 4000
-fpk=$4
-ppk=$5
-ssdPath=$6
-dataPath=$7
+ssdPath=$4
+dataPath=$5
+fpk=$6
+ppk=$7
 
 LOG=/tmp/chia-plot.log
+
+PARAMS="-k$kNum -n$roundNum -b$taskRam -t$ssdPath -d$dataPath"
+[ -z "$fpk" ] && PARAMS="$PARAMS -f$fpk"
+[ -z "$ppk" ] && PARAMS="$PARAMS -p$ppk"
 
 cd ~/chia-blockchain
 . ./activate
 
 echo "#############################################################"
-echo "chia plots create -k$kNum -n$roundNum -b$taskRam -f$fpk -p$ppk -t$ssdPath -d$dataPath" >> $LOG
+echo "chia plots create $PARMS" >> $LOG
 echo "#############################################################"
 
 STARTTIME=$(date +%s)
-chia plots create -k$kNum -n$roundNum -b$taskRam -f$fpk -p$ppk -t$ssdPath -d$dataPath
+chia plots create $PARAMS > /dev/null 2>&1
 ENDTIME=$(date +%s)
 
 echo "It takes $($ENDTIME - $STARTTIME) sec to complete this plotting" >> $LOG
--- playbooks/roles/deploy-chia/files/chia_plot_data_batch.sh
+++ playbooks/roles/deploy-chia/files/chia_plot_data_batch.sh
@@ -6,7 +6,7 @@ cd ~/chia-blockchain
 for i in {1..32}
 do
     #chia plots create -k$kNum -n$roundNum -b$taskRam -f$fpk -p$ppk -t$ssdPath -d$dataPath
-    sh ./chia_plot_data.sh 32 1 4000 $fpk $ppk $ssdPath $dataPath > /dev/null 2>&1 &
+    sh ./chia_plot_data.sh 32 1 4000 $fpk $ppk $ssdPath $dataPath &
 
     sleep 30
 done
