849b47e238a04ee157d987733ca80ffdbabf504c
tigerich <tigerich@163.com>
2021-05-20 16:05:53 +0800

    update join hpool

--- playbooks/roles/deploy-chia/files/chia_join_hpool.sh
+++ playbooks/roles/deploy-chia/files/chia_join_hpool.sh
@@ -6,5 +6,10 @@ function join_pool {
     ./hpool-miner-chia -config ./hpool_conf.yml
 }
 
+cnt1=$(ls /media/ray/data1/*.plot |wc -l)
+cnt2=$(ls /media/ray/data2/*.plot |wc -l)
+cnt=$(( $cnt1 + $cnt2 ))
+[ "$cnt" -eq 0 ] && exit 1
+
 hpool_cnt=$(ps aux |grep chia_join_hpool | wc -l)
 [[ "$hpool_cnt" -eq 1 ]] && join_pool
