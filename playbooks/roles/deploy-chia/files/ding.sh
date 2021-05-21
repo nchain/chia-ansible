f8b797d5139a4d9d334ef7ee407bab05dfc29e0a
tigerich <tigerich@163.com>
2021-05-21 16:49:21 +0800

    update

---
+++ playbooks/roles/deploy-chia/files/ding.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+function SendMessageToDingding(){ 
+    Dingding_Url="https://oapi.dingtalk.com/robot/send?access_token=2b8cf0781734a2db5cac5bc647169fe1fe7d5c25eab26f8dfd9e91d1c9870104" 
+    # 发送钉钉消息
+    curl "${Dingding_Url}" -H 'Content-Type: application/json' -d "
+    {
+        \"actionCard\": {
+            \"title\": \"$1\", 
+            \"text\": \"$2\", 
+            \"hideAvatar\": \"0\", 
+            \"btnOrientation\": \"0\", 
+            \"btns\": [
+                {
+                    \"title\": \"$1\", 
+                    \"actionURL\": \"\"
+                }
+            ]
+        }, 
+        \"msgtype\": \"actionCard\"
+    }"
+} 
+
+Subject="$1" 
+Body="$2"
+SendMessageToDingding $Subject $Body
--- playbooks/roles/deploy-chia/files/init.sh
+++ playbooks/roles/deploy-chia/files/init.sh
@@ -10,6 +10,8 @@ sudo mkdir -p /media/ray/ssd && sudo chown ray:ray /media/ray/ssd
 sudo mkdir -p /media/ray/data1 && sudo chown ray:ray /media/ray/data1
 sudo mkdir -p /media/ray/data2 && sudo chown ray:ray /media/ray/data2
 
+rm -rf /media/ray/ssd/*
+
 cd ~/chia-blockchain
 git remote set-url origin git@github.com:Chia-Network/chia-blockchain.git
 git reset --hard
