9678d2ac2202a346850c320b2ee36723b7ff7229
tigerich <tigerich@163.com>
2021-05-22 22:29:41 +0800

    update ding body

--- playbooks/roles/deploy-chia/files/ding.sh
+++ playbooks/roles/deploy-chia/files/ding.sh
@@ -1,14 +1,22 @@
 #!/bin/bash 
 
 DingdingUrl=https://oapi.dingtalk.com/robot/send?access_token=2b8cf0781734a2db5cac5bc647169fe1fe7d5c25eab26f8dfd9e91d1c9870104
-Subject=$1
-Body="Chia-offline:\n $2"
+subject=$1
+offlines=$2
+
+body='('
+for i in "${offlines[@]}"; do
+    body="$body $i"
+done
+body=')'
+
+body="Chia-offline:\n\n$body"
 
 curl $DingdingUrl -H 'Content-Type: application/json' --data-binary @- << EOF
 {
     "actionCard": {
-        "title": "$Subject", 
-        "text": "$Body", 
+        "title": "$subject", 
+        "text": "$body", 
         "hideAvatar": "0", 
         "btnOrientation": "0", 
         "btns": [
