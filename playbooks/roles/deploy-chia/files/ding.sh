8f7edf332b5098b6dde760d506ec91cbe1cbc02e
tigerich <tigerich@163.com>
2021-05-22 14:14:21 +0800

    update ding

--- playbooks/roles/deploy-chia/files/ding.sh
+++ playbooks/roles/deploy-chia/files/ding.sh
@@ -1,24 +1,25 @@
 #!/bin/bash 
 
-function SendMessageToDingding(){ 
+function SendMessageToDingding() { 
     Dingding_Url="https://oapi.dingtalk.com/robot/send?access_token=2b8cf0781734a2db5cac5bc647169fe1fe7d5c25eab26f8dfd9e91d1c9870104" 
     # 发送钉钉消息
-    curl "${Dingding_Url}" -H 'Content-Type: application/json' -d "
+    curl "${Dingding_Url}" -H 'Content-Type: application/json' --data-binary @- << EOF
     {
-        \"actionCard\": {
-            \"title\": \"$1\", 
-            \"text\": \"$2\", 
-            \"hideAvatar\": \"0\", 
-            \"btnOrientation\": \"0\", 
-            \"btns\": [
+        "actionCard": {
+            "title": "$1", 
+            "text": "$2", 
+            "hideAvatar": "0", 
+            "btnOrientation": "0", 
+            "btns": [
                 {
-                    \"title\": \"$1\", 
-                    \"actionURL\": \"\"
+                    "title": "$1", 
+                    "actionURL": ""
                 }
             ]
         }, 
-        \"msgtype\": \"actionCard\"
-    }"
+        "msgtype": "actionCard"
+    }
+    EOF
 } 
 
 Subject="$1" 
