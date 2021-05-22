8af4665b953c31cc0eb2421d44128884e2d67c7b
tigerich <tigerich@163.com>
2021-05-22 21:16:03 +0800

    update

--- playbooks/roles/deploy-chia/files/ding.sh
+++ playbooks/roles/deploy-chia/files/ding.sh
@@ -2,7 +2,7 @@
 
 DingdingUrl=https://oapi.dingtalk.com/robot/send?access_token=2b8cf0781734a2db5cac5bc647169fe1fe7d5c25eab26f8dfd9e91d1c9870104
 Subject=$1
-Body=$2
+Body="Chia-offline:\n $2"
 
 curl $DingdingUrl -H 'Content-Type: application/json' --data-binary @- << EOF
 {
