bf9736d9899b98a4e45b1972fa4f9775be6f9a26
tigerich <tigerich@163.com>
2021-05-22 23:05:55 +0800

    update

--- playbooks/roles/deploy-chia/files/checkon.sh
+++ playbooks/roles/deploy-chia/files/checkon.sh
@@ -3,16 +3,12 @@
 checkon="/home/ray/run-chia-ansible.sh ping"
 bash_ding="bash /home/ray/chia-ansible/playbooks/roles/deploy-chia/files/ding.sh"
 
-
 $checkon all > ~/checkon.log
 res=$(cat ./checkon.log |grep "unreachable=1" |cut -d ' ' -f1)
-echo $res
 
 IFS=$'\n' read -rd '' -a hosts <<<"$res"
 for host in "${hosts[@]}"; do
     offline_hosts="$offline_hosts $host"
 done
-
-echo $offline_hosts
 
 $bash_ding "【Chia机器下线报警】" "$offline_hosts"
--- playbooks/roles/deploy-chia/files/ding.sh
+++ playbooks/roles/deploy-chia/files/ding.sh
@@ -2,8 +2,8 @@
 
 DingdingUrl=https://oapi.dingtalk.com/robot/send?access_token=2b8cf0781734a2db5cac5bc647169fe1fe7d5c25eab26f8dfd9e91d1c9870104
 subject=$1
-body="Chia-offline:\n\n$2"
-echo $body >> /tmp/ding.log
+body="Chia Offline Hosts:\n\n$2"
+# echo $body >> /tmp/ding.log
 
 curl $DingdingUrl -H 'Content-Type: application/json' --data-binary @- << EOF
 {
