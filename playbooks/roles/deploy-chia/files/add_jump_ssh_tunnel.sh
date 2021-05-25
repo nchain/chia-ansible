8e475156d91bddadb1ee54df820ac1b9739f0c9e
tigerich <tigerich@163.com>
2021-05-25 13:27:38 +0800

    update jump ssh

--- playbooks/roles/deploy-chia/files/add_jump_ssh_tunnel.sh
+++ playbooks/roles/deploy-chia/files/add_jump_ssh_tunnel.sh
@@ -1,9 +1,13 @@
 # ssh -fN -R hn-jump:10022:localhost:22 richardchen@hn-jump 
 
-autossh -M 10021 -fN \
+AUTOSSH_CONF=~/autossh.conf
+
+source <(grep = "$AUTOSSH_CONF")
+
+autossh -M $mon_port -fN \
   -o "PubkeyAuthentication=yes" \
   -o "StrictHostKeyChecking=false" \
   -o "PasswordAuthentication=no" \
   -o "ServerAliveInterval 60" \
   -o "ServerAliveCountMax 3" \
-  -R hn-jump:10022:localhost:22 richardchen@hn-jump
+  -R hn-jump:$ssh_port:localhost:22 richardchen@hn-jump
